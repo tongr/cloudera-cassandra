5046c2265d6292e03a71f5b8de2de4bc1227f0af
tongr <tongr@online>
2017-01-27 13:42:47 +0100

    enable creating CSD with parcel urls

--- build_csd.sh
+++ build_csd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "" ]; then
-    echo "Usage: $0 <VERSION>"
+    echo "Usage: $0 <VERSION> [PARCEL_URL]"
     echo
     echo "Example: $0 1.0"
     exit 1
@@ -11,9 +11,21 @@ set -ex
 
 JARNAME=CASSANDRA-$1.jar
 
+
+if [ -z "$2" ]; then
+  CSD_SRC="./csd-src"
+else
+  # add parcel url to service description
+  URL="$2"
+  CSD_SRC="./csd-src-tmp"
+  rm -rf "$CSD_SRC"
+  cp -r "./csd-src" "$CSD_SRC"
+  sed -ie "s|\"parcel\" : {|\"parcel\" :\n   {\n   \t\t\"repoUrl\" : \"$URL\",|" "$CSD_SRC/descriptor/service.sdl"
+fi
+
 # validate service description
-java -jar cm_ext/validator/target/validator.jar -s ./csd-src/descriptor/service.sdl
+java -jar cm_ext/validator/target/validator.jar -s "$CSD_SRC/descriptor/service.sdl"
 
 mkdir -p ./parcel-and-csd
-jar -cvf ./parcel-and-csd/$JARNAME -C ./csd-src .
+jar -cvf ./parcel-and-csd/$JARNAME -C "$CSD_SRC" .
 echo "Created $JARNAME"
